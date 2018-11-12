4ac059732a597c146eed4f02426b50d53d2e0db9
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2018-11-12 02:51:41 -0600

    Modify FieldSynthesisTheorem to accept an arbitrary line profile Add Proof reference to README.md

--- FieldSynthesis/FieldSynthesisTheorem.m
+++ FieldSynthesis/FieldSynthesisTheorem.m
@@ -1,4 +1,4 @@
-function [efield,slice,smear,Q,T] = FieldSynthesisTheorem(efield)
+function [F,fieldSynthesis,dithered,Q,T] = FieldSynthesisTheorem(F,L)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Small program to illustrate a new Field Synthesis Theorem.
 %
@@ -13,11 +13,11 @@ function [efield,slice,smear,Q,T] = FieldSynthesisTheorem(efield)
 %   Mark Kittisopikul, May 2017 - Aug 2018
 %
 %   INPUT
-%   efield - electric field at the focal plane, may be real or complex
+%   F - electric field at the front focal plane, may be real or complex
 %            valued
 %   
 %   OUTPUT
-%   efield - electric field at the focal plane
+%   F - electric field at the front focal plane
 %   slice  - intensity of illumination pattern by field synthesis
 %   smear  - intensity of illumination pattern by dithering
 %   Q      - Fourier transform of individual line scan without phasing,a=10
@@ -47,118 +47,154 @@ function [efield,slice,smear,Q,T] = FieldSynthesisTheorem(efield)
 
 %% Initialize input
 % assume this is the e-field in real space (real or complex valued)
-if(nargin < 1)
+if(nargin < 1 || isempty(F))
     % Use stock image as an arbitrary efield
-    efield = imread('cameraman.tif');
+    F = imread('cameraman.tif');
     imaginaryAmplitude = 1;
-    efield=double(efield)+1j*rand(size(efield))*imaginaryAmplitude;
+    F=double(F)+1j*rand(size(F))*imaginaryAmplitude;
+end
+if(nargin < 2 || isempty(L))
+    L = ones(1,size(F,1));
 end
 
-% efield is F(x,y) in the proof and can be complex valued
-% get efield properties
-sz = size(efield);
+% F(x,y) is the eletric field and can be complex valued
+% get F properties. F was previously called efield
+sz = size(F);
 N = sz(2);
+center = floor(N/2+1);
 
 % then this is the spectrum of the efield
 % spectrum is F_hat(k_x,k_y) in the proof
-spectrum_unshifted = fft2(efield);
-spectrum=fftshift(spectrum_unshifted);
+F_hat_unshifted = fft2(F);
+F_hat=fftshift(F_hat_unshifted);
 % the intensity is the modulus squared of the efield
 % this is a real valued image         
-I=abs(efield).^2;                         
+F_sqmod=abs(F).^2;                         
 % slice is an array that is created by superposition of
 % inverse FFTs of spectral slices 
-slice=zeros(sz);
+fieldSynthesis=zeros(sz);
 % smear is created by scanning the image in real space                  
-smear=zeros(sz);                      
+dithered=zeros(sz);                      
+
+%% Calculate line scan profile at back focal plane
+L_hat_unshifted = fft(ifftshift(L));
+L_hat = fftshift(L_hat_unshifted);
+L_hat = repmat(L_hat,size(F,1),1);
+
+%% Calculate convolution kernel for front focal plane
+L_sqmod = zeros(size(F));
+L_sqmod(center,:) = abs(L).^2;
  
 %% Do line scan
 for a=1:N
+    
+    % If L were a delta function, then this would work
     % take one slice of spectrum and take inverse FFT
-    T_hat=zeros(sz);
-    T_hat(:,a)=spectrum(:,a);
+    % T_hat=zeros(sz);
+    % T_hat(:,a)=F_hat(:,a);
+    
+    T_hat = F_hat.*circshift(L_hat,[0 a-center]);
+
     T=ifft2(ifftshift(T_hat));
     % superimpose intensities (modulus squared) of
     % inverse FFTs of spectral slices          
-    slice=slice+abs(T).^2;           
+    fieldSynthesis=fieldSynthesis+abs(T).^2;           
     % smearing the image in x-direction
     % intensity is superimposed at every position
-    smear=smear+(circshift(I,[0,a]));
+    dithered=dithered+(circshift(F_sqmod,[0,a-center])).*L_sqmod(center,a);
 end
 
-% We could also create smear as follows:
-% smear = repmat(sum(I,2),1,256);
+
 
 %% Line profiles of the smeared intensity images
  
-sliceProfile=slice(:,1);
+fieldSynthesisCentralZSlice=fieldSynthesis(:,center);
  
-smearProfile=smear(:,1)/N;
+ditheredCentralZSlice=dithered(:,center);
  
 %alternatively, one can also just project the intensity image I in x
-projectionProfile=sum(I,2)/N;
+% if L_sqmod were delta(z)
+% projectionProfile=sum(F_sqmod,2)/N;
+% more generally, this can be done by circular convolution
+smearByCircularConvolution = fftshift(ifft2(fft2(ifftshift(F_sqmod)).*fft2(ifftshift(L_sqmod))));
+
 
 % The profile can be calculated directly by taking a 1 dimensional fourier
 % transform
-oneDFT = ifft(spectrum_unshifted)/N;
-% This line is equivalent to the above
-% oneDFT = fft(efield,[],2)/N;
+F_unshifted = ifftshift(F,2);
+if(mod(N,2) == 0)
+    % even
+    L_flipped_unshifted = [L(1) fliplr(L(2:end))];
+else
+    % odd
+    L_flipped_unshifted = fliplr(L);
+end
+L_flipped_unshifted = repmat(L_flipped_unshifted,N,1);
+L_flipped_unshifted = ifftshift(L_flipped_unshifted,2);
+
+oneDFT = fftshift(fft(F_unshifted.*L_flipped_unshifted,[],2),2);
 oneDFT = abs(oneDFT).^2;
 oneDFT = sum(oneDFT,2);
 
  
 %% Plot Figures;
 figure;
+
 subplot(2,3,1);
-imshow(I,[]);
+imshow(F_sqmod,[]);
 title('Original Intensity');
+
 subplot(2,3,2);
-imshow(slice,[]);
-title('Slice');
+imshow(fieldSynthesis,[]);
+title('Field Synthesis');
+
 subplot(2,3,3);
-imshow(smear,[]);
-title('Smear (~Dither)');
+imshow(dithered,[]);
+title('Smear / Dither');
 
  
 % All profiles are identical
 subplot(2,3,4:6);
-plot(projectionProfile,'ko','DisplayName','Projection')
+plot(smearByCircularConvolution(:,center),'ko','DisplayName','Projection')
 hold on;
-xlim([1 256]);
+xlim([1 N]);
 
-plot(sliceProfile,'b+','DisplayName','Slice');
+plot(fieldSynthesisCentralZSlice/N,'b+','DisplayName','Field Synthesis');
 
-plot(smearProfile,'rx','DisplayName','Smear');
+plot(ditheredCentralZSlice,'rx','DisplayName','Dithered');
 
-plot(oneDFT,'g.','DisplayName','1D FT');
+plot(oneDFT/N,'g.','DisplayName','1D FT');
 
 grid on;
 legend('show','Location','southwest');
-title('Vertical Profiles are the Same');
+title('Vertical Z Profiles are the Identical');
 xlabel('z position');
 
-disp('Press any key');
-pause;
+% disp('Press any key');
+% pause;
+return;
 
 %% Explanation of the profile of individual line scans
 % T represents a selected column in the spectral field selected by the scan
 
-efield_xft = fft(efield,[],2);
+% efield_xft = fft(F,[],2);
  
 % for k=1:N
 
-a = 10;
+a = center-10;
 hfig = figure('units','normalized','outerposition',[0 0 1 1]);
 
 % T is constructed similarly to above in the for loop
 % The only difference is how k is indexed in the unshifted spectrum
-T_hat=zeros(sz);
-T_hat(:,a)=spectrum_unshifted(:,a);
+% T_hat=zeros(sz);
+% T_hat(:,a)=F_hat_unshifted(:,a);
+T_hat = F_hat_unshifted.*circshift(L_hat_unshifted,[0 a-center]);
 T = ifft2(T_hat);
 
 % Q differs from T because the selected column is copied into k_x = 0
-Q_hat = zeros(N);
-Q_hat(:,1) = spectrum_unshifted(:,a);
+% Q_hat = zeros(N);
+% Q_hat(:,1) = F_hat_unshifted(:,a);
+Q_hat = circshift(T_hat,[0 center-a]);
 Q = ifft2(Q_hat);
 
 subplot(4,3,1);
@@ -188,13 +224,15 @@ title('abs(T)^2');
 subplot(4,3,7:9);
 plot(real(Q(:,1))*N,'r-');
 hold on;
-plot(real(efield_xft(:,a)),'ro');
+% plot(real(efield_xft(:,a)),'ro');
  
 plot(imag(Q(:,1))*N,'b-');
-plot(imag(efield_xft(:,a)),'bo');
+% plot(imag(efield_xft(:,a)),'bo');
  
-legend({'real(Q)','Real 1D FFT of E','imag(Q)','Imag 1D FFT of E'}, ...
-    'Location','southoutside','Orientation','horizontal');
+% legend({'real(Q)','Real 1D FFT of E','imag(Q)','Imag 1D FFT of E'}, ...
+%     'Location','southoutside','Orientation','horizontal');
+legend({'real(Q)','imag(Q)'}, ...
+     'Location','southoutside','Orientation','horizontal');
 title('Q is the Fourier Transform of a Line Scan in the Spectrum');
 xlim([1 N]);
 xlabel('z position (pixels)');
@@ -203,11 +241,11 @@ xlabel('z position (pixels)');
 % pause(1);
  
 subplot(4,3,10:12);
-plot(real(exp(1i*2*pi*(a-1)/N.*(0:N-1))),'r-');
+% plot(real(exp(1i*2*pi*(a-1)/N.*(0:N-1))),'r-');
 hold on;
-plot(real(T(1,:)./Q(1,:)),'ro');
-plot(imag(exp(1i*2*pi*(a-1)/N.*(0:N-1))),'b-');
-plot(imag(T(1,:)./Q(1,:)),'bo');
+plot(real(T(center,:)./Q(center,:)),'ro');
+% plot(imag(exp(1i*2*pi*(a-1)/N.*(0:N-1))),'b-');
+plot(imag(T(center,:)./Q(center,:)),'bo');
 title(['T is Q With Complex Modulation ' ...
     'Due to the Location of the Line Scan']);
 xlabel('z position (pixels)');
--- README.md
+++ README.md
@@ -19,6 +19,11 @@ Bo-Jui Chang<sup>1</sup>, Mark Kittisopikul<sup>2,4</sup>, Kevin M. Dean<sup>1,3
 ### BioRxiv Preprint
 [https://www.biorxiv.org/content/early/2018/09/26/427468](https://www.biorxiv.org/content/early/2018/09/26/427468)
 
+## Proof
+
+The canonical proof can be found in the supplemental information of the manuscript. Here we also have also included 
+[https://mkitti.github.io/FieldSynthesis/FieldSynthesis/FieldSynthesisProofIllustration.html](an illustrated proof for the Field Synthesis Theorem in the form of a MATLAB Live Script).
+
 ## System Requirements
 
 * [MATLAB (2017a or above), Mathworks, Natick, MA](https://www.mathworks.com/support/sysreq.html)
